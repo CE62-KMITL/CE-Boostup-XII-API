66f6c1d5e75e5cbdff6c975378ae888e30579cd5
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2024-05-01 22:37:34 +0700

    Add missing ) to user total score query

--- api/src/users/entities/user.entity.ts
+++ api/src/users/entities/user.entity.ts
@@ -31,7 +31,7 @@ export class User {
 
   @Formula(
     (alias) =>
-      `(SELECT SUM(\`score\`) FROM \`problem\` WHERE \`problem\`.\`id\` IN (SELECT DISTINCT \`problem_id\` FROM \`submission\` WHERE \`submission\`.\`user_id\` = ${alias}.\`id\` AND \`submission\`.\`accepted\` = 1)`,
+      `(SELECT SUM(\`score\`) FROM \`problem\` WHERE \`problem\`.\`id\` IN (SELECT DISTINCT \`problem_id\` FROM \`submission\` WHERE \`submission\`.\`user_id\` = ${alias}.\`id\` AND \`submission\`.\`accepted\` = 1))`,
   )
   totalScore: number;
 
