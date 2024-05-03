3dbc1e844257129145f4605e3d7051e8051625e9
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-05-03 15:16:46 +0700

    authentication controller outline

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, NotImplementedException, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('login')
+  login() {
+    throw new NotImplementedException();
+  }
+
+  @Post('request-account-creation')
+  requestAccountCreation() {
+    throw new NotImplementedException();
+  }
+
+  @Post('create-account')
+  createAccount() {
+    throw new NotImplementedException();
+  }
+
+  @Post('request-password-reset')
+  requestPasswordReset() {
+    throw new NotImplementedException();
+  }
+
+  @Post('reset-password')
+  resetPassword() {
+    throw new NotImplementedException();
+  }
+}
