cf2a02feffea6f4004e6e1ec76605d83d747756f
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-05-04 23:23:08 +0700

    update constants so it's align with the database

--- api/src/auth/constants.ts
+++ api/src/auth/constants.ts
@@ -1,2 +1,3 @@
 export const MIN_PASSWORD_LENGTH = 8;
-export const MAX_PASSWORD_LENGTH = 128;
+export const MAX_PASSWORD_LENGTH = 255;
+export const MAX_DISPLAY_NAME_LENGTH = 32;
--- api/src/auth/dto/create-account.dto.ts
+++ api/src/auth/dto/create-account.dto.ts
@@ -1,7 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
+import { MAX_DISPLAY_NAME_LENGTH } from 'src/auth/constants';
 import { ResetPasswordDto } from 'src/auth/dto/reset-password.dto';
 
 export class CreateAccountDto extends ResetPasswordDto {
   @IsString()
+  @MaxLength(MAX_DISPLAY_NAME_LENGTH)
   displayName: string;
 }
