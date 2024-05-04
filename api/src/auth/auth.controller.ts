4f02fd41c23103d46448ad4044d0c9bb2cee1278
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-05-04 15:25:38 +0700

    echos back request body while waiting for implemantation

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,29 +1,43 @@
-import { Controller, NotImplementedException, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  NotImplementedException,
+  Post,
+} from '@nestjs/common';
+import { CreateAccountDto } from 'src/auth/dto/create-account.dto';
+import { LoginDto } from 'src/auth/dto/login.dto';
+import { RequestAccountCreationDto } from 'src/auth/dto/request-account-creation.dto';
+import { RequestPasswordResetDto } from 'src/auth/dto/request-password-reset.dto';
+import { ResetPasswordDto } from 'src/auth/dto/reset-password.dto';
 
 @Controller('auth')
 export class AuthController {
   @Post('login')
-  login() {
-    throw new NotImplementedException();
+  login(@Body() loginDto: LoginDto) {
+    throw new NotImplementedException(loginDto);
   }
 
   @Post('request-account-creation')
-  requestAccountCreation() {
-    throw new NotImplementedException();
+  requestAccountCreation(
+    @Body() requestAccountCreationDto: RequestAccountCreationDto,
+  ) {
+    throw new NotImplementedException(requestAccountCreationDto);
   }
 
   @Post('create-account')
-  createAccount() {
-    throw new NotImplementedException();
+  createAccount(@Body() createAccountDto: CreateAccountDto) {
+    throw new NotImplementedException(createAccountDto);
   }
 
   @Post('request-password-reset')
-  requestPasswordReset() {
-    throw new NotImplementedException();
+  requestPasswordReset(
+    @Body() requestPasswordResetDto: RequestPasswordResetDto,
+  ) {
+    throw new NotImplementedException(requestPasswordResetDto);
   }
 
   @Post('reset-password')
-  resetPassword() {
-    throw new NotImplementedException();
+  resetPassword(@Body() resetPasswordDto: ResetPasswordDto) {
+    throw new NotImplementedException(resetPasswordDto);
   }
 }
