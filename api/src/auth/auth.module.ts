e233cde6f8f076e1b21ca7db65de11961b4634a2
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-05-05 23:37:35 +0700

    half baked prototype draft auth service

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,11 +1,18 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from 'src/auth/auth.service';
 import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([User])],
+  imports: [
+    MikroOrmModule.forFeature([User]),
+    JwtModule.register({
+      privateKey: process.env.JWT_ES256_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,45 +1,167 @@
 import { EntityManager, EntityRepository } from '@mikro-orm/mariadb';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import {
+  BadRequestException,
   Injectable,
   NotImplementedException,
-  UnauthorizedException,
 } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as argon2 from 'argon2';
+import type {
+  JWTPayload,
+  JWTPayloadCreateAccount,
+  JWTPayloadResetPassword,
+} from 'src/auth/auth';
+import { CreateAccountDto } from 'src/auth/dto/create-account.dto';
 import { LoginDto } from 'src/auth/dto/login.dto';
+import { RequestAccountCreationDto } from 'src/auth/dto/request-account-creation.dto';
+import { RequestPasswordResetDto } from 'src/auth/dto/request-password-reset.dto';
+import { ResetPasswordDto } from 'src/auth/dto/reset-password.dto';
 import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
+    private readonly jwtService: JwtService,
+
     @InjectRepository(User)
     private readonly usersRepository: EntityRepository<User>,
     private readonly entityManager: EntityManager,
   ) {}
 
-  async findOne(loginDto: LoginDto) {
-    const findOneOptions = {
-      email: loginDto.email,
+  createJWTPayload(user: User): JWTPayload {
+    return {
+      sub: user.id,
+      email: user.email,
     };
-    const user = await this.usersRepository.findOne(findOneOptions);
+  }
 
+  sendEmail(link: string) {
+    throw new NotImplementedException(link);
+  }
+
+  async findOneByEmail(email: string) {
+    return await this.usersRepository.findOne({ email });
+  }
+
+  async login(loginDto: LoginDto) {
+    const user = await this.findOneByEmail(loginDto.email);
     if (!user || !user.hashedPassword) {
-      throw new UnauthorizedException();
+      throw new BadRequestException({
+        message: 'Invalid email or password',
+      });
     }
 
     const correctPassword = await argon2.verify(
       user.hashedPassword,
       loginDto.password,
     );
-
-    if (correctPassword) {
-      return user;
+    if (!correctPassword) {
+      throw new BadRequestException({
+        message: 'Invalid email or password',
+      });
     }
+
+    const token = this.jwtService.sign(this.createJWTPayload(user));
+    return {
+      token: token,
+    };
   }
 
-  async login(loginDto: LoginDto) {
-    const user = await this.findOne(loginDto);
-    // TODO: generate jwt and returns it
-    throw new NotImplementedException(user);
+  async requestAccountCreation(
+    requestAccountCreationDto: RequestAccountCreationDto,
+  ) {
+    const user = await this.findOneByEmail(requestAccountCreationDto.email);
+
+    if (!user || user.hashedPassword !== '') {
+      throw new BadRequestException({
+        message: 'Invalid email',
+      });
+    }
+
+    const payload: JWTPayloadCreateAccount = {
+      ...this.createJWTPayload(user),
+      createAccount: true,
+    };
+    const token = this.jwtService.sign(payload);
+    const link = `${requestAccountCreationDto.siteUrl}/auth/create-account?token=${token}`;
+    this.sendEmail(link);
+
+    return {
+      message: 'Account creation email has been sent',
+    };
+  }
+
+  async createAccount(createAccountDto: CreateAccountDto) {
+    const payload: JWTPayloadCreateAccount = this.jwtService.verify(
+      createAccountDto.token,
+    );
+    if (!payload.createAccount) {
+      throw new BadRequestException({
+        message: 'Invalid token or displayName',
+      });
+    }
+
+    const user = await this.findOneByEmail(payload.email);
+    if (!user) {
+      throw new BadRequestException({
+        message: 'Invalid token or displayName',
+      });
+    }
+
+    user.hashedPassword = await argon2.hash(createAccountDto.password);
+    user.displayName = createAccountDto.displayName;
+    await this.entityManager.flush();
+
+    return {
+      message: 'Account has been created',
+    };
+  }
+
+  async requestPasswordReset(requestPasswordResetDto: RequestPasswordResetDto) {
+    const user = await this.findOneByEmail(requestPasswordResetDto.email);
+
+    if (!user) {
+      throw new BadRequestException({
+        message: 'Invalid email',
+      });
+    }
+
+    const payload: JWTPayloadResetPassword = {
+      ...this.createJWTPayload(user),
+      resetPassword: true,
+    };
+    const token = this.jwtService.sign(payload);
+    const link = `${requestPasswordResetDto.siteUrl}/auth/reset-password?token=${token}`;
+    this.sendEmail(link);
+
+    return {
+      message: 'Password reset email has been sent',
+    };
+  }
+
+  async resetPassword(resetPasswordDto: ResetPasswordDto) {
+    const payload: JWTPayloadCreateAccount = this.jwtService.verify(
+      resetPasswordDto.token,
+    );
+    if (!payload.createAccount) {
+      throw new BadRequestException({
+        message: 'Invalid token',
+      });
+    }
+
+    const user = await this.findOneByEmail(payload.email);
+    if (!user) {
+      throw new BadRequestException({
+        message: 'Invalid token',
+      });
+    }
+
+    user.hashedPassword = await argon2.hash(resetPasswordDto.password);
+    await this.entityManager.flush();
+
+    return {
+      message: 'Password has been reset',
+    };
   }
 }
