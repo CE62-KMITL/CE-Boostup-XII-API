f80c85230c8d36ecae2aba65c7dc5ee77627540d
Nat-thapas <103213621+Nat-thapas@users.noreply.github.com>
2024-06-18 09:37:00 +0700

    Add temporary performace logging

--- api/src/problems/problems.service.ts
+++ api/src/problems/problems.service.ts
@@ -122,6 +122,8 @@ export class ProblemsService implements OnModuleInit {
     originUser: AuthenticatedUser,
     findAllDto: FindAllDto,
   ): Promise<PaginatedResponse<ProblemResponse>> {
+    console.log('start'); // TODO: Remove
+    let start = performance.now(); // TODO: Remove
     const user = await this.usersService.findOneInternal({ id: originUser.id });
     if (!user) {
       throw new UnauthorizedException({
@@ -129,6 +131,8 @@ export class ProblemsService implements OnModuleInit {
         errors: { token: 'Invalid token' },
       });
     }
+    console.log('get user took', performance.now() - start); // TODO: Remove
+    start = performance.now(); // TODO: Remove
     const where: FilterQuery<Problem> = {};
     if (findAllDto.search) {
       where.$or = [{ title: { $like: `%${findAllDto.search}%` } }];
@@ -172,7 +176,11 @@ export class ProblemsService implements OnModuleInit {
     const offset: number = (findAllDto.page - 1) * findAllDto.perPage;
     const limit: number = findAllDto.perPage;
     let orderBy: Record<string, 'asc' | 'desc'> | null = null;
+    console.log('conditions build took', performance.now() - start); // TODO: Remove
+    start = performance.now(); // TODO: Remove
     const completionStatuses = await this.getCompletionStatuses(user);
+    console.log('get completion statuses took', performance.now() - start); // TODO: Remove
+    start = performance.now(); // TODO: Remove
     if (
       isSomeRolesIn(originUser.roles, [Role.Staff, Role.Admin, Role.SuperAdmin])
     ) {
@@ -196,6 +204,8 @@ export class ProblemsService implements OnModuleInit {
           'updatedAt',
         ]);
       }
+      console.log('sort parsing took', performance.now() - start); // TODO: Remove
+      start = performance.now(); // TODO: Remove
       if (orderBy) {
         const [problems, count] = await this.problemsRepository.findAndCount(
           where,
@@ -206,6 +216,7 @@ export class ProblemsService implements OnModuleInit {
             orderBy,
           },
         );
+        console.log('find and count took', performance.now() - start); // TODO: Remove
         return {
           data: problems.map(
             (problem) =>
@@ -229,6 +240,7 @@ export class ProblemsService implements OnModuleInit {
           limit,
         },
       );
+      console.log('find and count took', performance.now() - start); // TODO: Remove
       return {
         data: problems.map(
           (problem) =>
